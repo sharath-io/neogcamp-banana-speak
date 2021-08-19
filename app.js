86905abb15b5c2fda259af3da8f6e6857ba98714
Sharath <sharath.esr@gmail.com>
2021-08-19 15:48:18 +0530

    added URL and done

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ var btnTranslate =document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL = "https://api.funtranslations.com/translate/groot.json";
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorhandler(error){
+    console.log("error occured", error);
+    alert(" Server not responding Try after sometime");
+}
 
 function clickEventHandler()
 {
-    outputDiv.innerText ="##$%^&&&^$@" + txtInput.value;
+    var inputText = txtInput.value;   //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText; //output
+        })
+    .catch(errorhandler);
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);
